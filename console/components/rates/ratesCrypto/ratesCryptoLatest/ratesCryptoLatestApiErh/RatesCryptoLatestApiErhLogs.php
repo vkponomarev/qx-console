11a30e75f91f06f3b721e4ffd9f070b1b1e694a5
vkponomarev <vkponomarev@gmail.com>
2021-10-29 15:41:36 +0500

    Доработка todo

--- console/components/rates/ratesCrypto/ratesCryptoLatest/ratesCryptoLatestApiErh/RatesCryptoLatestApiErhLogs.php
+++ console/components/rates/ratesCrypto/ratesCryptoLatest/ratesCryptoLatestApiErh/RatesCryptoLatestApiErhLogs.php
@@ -26,50 +26,11 @@ class RatesCryptoLatestApiErhLogs
      */
     function __construct($apiErhDelete)
     {
-
-        $logErrorApiAccess = $apiErhDelete->apiErhInsert->apiErhRead->logErrorApiAccess;
-        $logErrorApiException = $apiErhDelete->apiErhInsert->apiErhRead->logErrorApiException;
-        $ApiResponse = $apiErhDelete->apiErhInsert->apiErhRead->apiResponse;
-        $config = $apiErhDelete->apiErhInsert->apiErhRead->config;
-
-        if ($logErrorApiAccess){
-            $model = new RatesLogs();
-            $model->api_id = 1;
-            $model->error = 1;
-            $model->component = get_class($this);
-            $model->message = 'Не удалось получить доступ к API ' . $config->organizationsApi;
-            $model->date_time = (new \DateTime('now'))->format('Y-m-d H:i:s');
-            $model->save();
-            print_r($model->message . "\r\n");
-
-        }
-
-        if ($logErrorApiException){
-            $model = new RatesLogs();
-            $model->api_id = 1;
-            $model->error = 1;
-            $model->component = get_class($this);
-            $model->message = $logErrorApiException;
-            $model->date_time = (new \DateTime('now'))->format('Y-m-d H:i:s');
-            $model->save();
-            print_r($model->message . "\r\n");
-
-        }
-
-        if ($ApiResponse){
-            $model = new RatesLogs();
-            $model->api_id = 1;
-            $model->error = 0;
-            $model->component = get_class($this);
-            $model->message = 'Обновление rates_currencies_latest последних курсов валют прошло успешно ' . $config->organizationsApi;
-            $model->date_time = (new \DateTime('now'))->format('Y-m-d H:i:s');
-            $model->save();
-            print_r($model->message . "\r\n");
-
-        }
-
-
-
+        /**
+         * @todo
+         * Необходимо записать логи в таблицу rates_logs
+         * По аналогии с class RatesCurrenciesLatestApiErhLogs
+         */
     }
 }
 
